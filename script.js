e5ea689c5ff411f6832d60cce1c3611c157e34d9
Amit Roy <amitroy2930@gmail.com>
2023-11-01 09:15:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Get the buttons and sections
+const onEducation = document.getElementById("onEducation");
+const onExperience = document.getElementById("onExperience");
+const educationSection = document.getElementById("education");
+const experienceSection = document.getElementById("experience");
+
+// Add click event listeners to the buttons
+onEducation.addEventListener("click", () => {
+  experienceSection.style.display = "none";
+  educationSection.style.display = "block";
+});
+
+onExperience.addEventListener("click", () => {
+  educationSection.style.display = "none";
+  experienceSection.style.display = "block";
+});

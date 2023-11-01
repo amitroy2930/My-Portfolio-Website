1efbed10bc5d9576295f9ea53cf736b7aa1deced
Amit Roy <amitroy2930@gmail.com>
2023-11-01 14:31:41 +0530

    contact form connected with googlesheet

--- contact-form-script.js
+++ contact-form-script.js
@@ -1,11 +1,18 @@
 // https://github.com/jamiewilson/form-to-google-sheets
 
+const msg = document.getElementById("msg")
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwZgQi1gOaoVSx9HBWs8WAYyugArmijGpXQE0QRovNZu_xbDIRWW7TAvbySS0mm0gzF9w/exec'
-  const form = document.forms['submit-to-google-sheet']
+const form = document.forms['submit-to-google-sheet']
 
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
+      .then(response => {
+        msg.innerHTML = "Your Message Has Been Successfully Sent."
+        setTimeout(() => {
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+      })
       .catch(error => console.error('Error!', error.message))
   })
